253b2dfbca67d4a3b9bd77d6a78087fb90e7415b
HarolDeveloper <hlancherosalvarez@gmail.com>
2025-04-25 14:34:04 -0600

    Initial Commit

---
+++ DevOS/DevOSApp.swift
@@ -0,0 +1,32 @@
+//
+//  DevOSApp.swift
+//  DevOS
+//
+//  Created by H Lancheros Alvarez on 25/04/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct DevOSApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
